fd50153cb8d1c92992b68fb03634d075783bf5ca
MD ABDULLAH <mdabdullah161036@gmail.com>
2025-08-28 12:54:04 +0600

    heart count added

--- js/index.js
+++ js/index.js
@@ -8,3 +8,23 @@ toggleBtn.addEventListener("click", () => {
   openIcon.classList.toggle("hidden");
   closeIcon.classList.toggle("hidden");
 });
+
+function getTextInNumber(id) {
+  const text = document.getElementById(id).innerText;
+  const convertInNumber = parseInt(text);
+  return convertInNumber;
+}
+function setTextInNumber(text, id) {
+  const TextId = document.getElementById(id);
+  TextId.innerText = text;
+}
+// heart count start
+const whiteHeart = document.getElementsByClassName("white-heart");
+for (const heart of whiteHeart) {
+  const item = heart.addEventListener("click", function () {
+    const availableHeart = getTextInNumber("available-heart");
+    const newHeart = availableHeart + 1;
+    setTextInNumber(newHeart, "available-heart");
+  });
+}
+// heart count end
